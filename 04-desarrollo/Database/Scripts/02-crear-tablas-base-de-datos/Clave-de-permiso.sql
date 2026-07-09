7877110064e08e4be5a367278c57d9308621fa8a
orozcoadrian-dev <adrianenriqueorozco@gmail.com>
2026-07-09 14:13:00 -0500

    fix: ajuste de atributo de la tabla clave de permiso.

--- 04-desarrollo/Database/Scripts/02-crear-tablas-base-de-datos/Clave-de-permiso.sql
+++ 04-desarrollo/Database/Scripts/02-crear-tablas-base-de-datos/Clave-de-permiso.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Clave_de_permiso
 (
-	Id_clave INT PRIMARY KEY,
+	Id_clave INT PRIMARY KEY AUTO_INCREMENT,
 	Usuario_id INT,
     Clave VARCHAR(30) NOT NULL UNIQUE,
     Fecha_creacion DATE NOT NULL,
