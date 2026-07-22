3aec38bf96550cbb484dbcc6f8dae8a559665b6e
orozcoadrian-dev <adrianenriqueorozco@gmail.com>
2026-07-22 09:54:29 -0500

    chore: actualización de archivos para finalizar la tarea. los archivos actualizados fueron:
        1. Config/ConexionBD.java: el cual se le implementó la documentación javadoc.
        2. Backend/.gitignore: fueron agregados archivos y carpetas para que se ignoren en el transcurso de desarrollo (Sea porque aún no están listos o porque no quiero que estén todavía en el repositorio remoto).

--- 04-desarrollo/Src/Backend/Config/ConexionBD.java
+++ 04-desarrollo/Src/Backend/Config/ConexionBD.java
@@ -1,22 +1,37 @@
 package Backend.Config;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexionBD extends Object
-{
-	// Variables como user, password, port y host, pueden cambiar según la configuración de entorno del desarrollador.
+public class ConexionBD extends Object {
+	// Variables como user, password, port y host, pueden cambiar según la
+	// configuración de entorno del desarrollador.
 	private static final String USER = "root";
 	private static final String PASSWORD = "";
 	private static final String DATABASE = "papeleria_matrix";
 	private static final String PORT = "3306";
 	private static final String HOST = "localhost";
-	private static final String URL = "jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE;
+	private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
 
-	public static Connection establecerConexion() throws SQLException
-	{
-		Connection conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-		System.out.println(DATABASE + " pudo conectarse exitosamente.");
+	/**
+	 * Establece y retorna una conexión activa con la base de datos utilizando las credenciales configuradas.
+	 * @author Orozco Adrián Dev.
+	 * @param none
+	 * @return {@code Connection}
+	 * @throws SQLException
+	 */
+	public static Connection establecerConexion() throws SQLException {
+
+		Connection conexion = null;
+
+		try {
+			conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println(DATABASE + " pudo conectarse exitosamente.");
+		} catch (SQLException e) {
+			System.err.println("Error al conectar con la base de datos: " + e.getMessage());
+			throw e;
+		}
 
 		// Devolvemos la conexión
 		return conexion;
